91858c048c5eede40b9db70f35736d54a02cae41
abiFarhan <abifarhan04@gmail.com>
2021-05-07 14:55:18 +0700

    Penambahan Dependecies dan Integrasi Firebase

--- app/src/main/java/com/mahia/ribot/SplashScreenActivity.kt
+++ app/src/main/java/com/mahia/ribot/SplashScreenActivity.kt
@@ -1,6 +1,6 @@
 package com.mahia.ribot
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashScreenActivity : AppCompatActivity() {
