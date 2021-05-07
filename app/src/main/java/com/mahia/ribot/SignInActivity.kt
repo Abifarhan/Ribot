4d839dbd400c0509b26895bdcdd6eb585fe6a5f1
abiFarhan <abifarhan04@gmail.com>
2021-05-07 14:15:21 +0700

    Penambahan activity splash dan sign in

--- app/src/main/java/com/mahia/ribot/SignInActivity.kt
+++ app/src/main/java/com/mahia/ribot/SignInActivity.kt
@@ -3,7 +3,7 @@ package com.mahia.ribot
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/mahia/ribot/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.mahia.ribot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
