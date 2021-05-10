780e24a3e10d8db80a42548b6d5b17f29b610231
abiFarhan <abifarhan04@gmail.com>
2021-05-10 23:21:52 +0700

    Pembuatan dan Modifikasi Bottom Navigation

--- app/src/main/java/com/mahia/ribot/view/auth/SignInActivity.kt
+++ app/src/main/java/com/mahia/ribot/view/auth/SignInActivity.kt
@@ -1,12 +1,13 @@
-package com.mahia.ribot
+package com.mahia.ribot.view.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mahia.ribot.R
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in)
 
     }
 }
--- app/src/main/java/com/mahia/ribot/view/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/mahia/ribot/view/splash/SplashScreenActivity.kt
@@ -1,4 +1,4 @@
-package com.mahia.ribot
+package com.mahia.ribot.view.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
